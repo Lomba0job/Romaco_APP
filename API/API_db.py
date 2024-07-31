aabb107b371342a83c25abe18e1c0058c73f0135
Michele Lombardi <michelelombardi.job@gmail.com>
2024-07-31 22:59:31 +0200

    modifica che avevo considerato ma non implementato

--- API/API_db.py
+++ API/API_db.py
@@ -32,7 +32,7 @@ import sqlite3
 
 def put(peso_tot, b1, b2, b3, b4, b5, b6, desc, prio, data):
     # Connessione al database (crea il database se non esiste)
-    conn = sqlite3.connect('pesata.db')
+    conn = sqlite3.connect(f.get_db())
     
     # Creazione di un cursore per eseguire i comandi SQL
     cursor = conn.cursor()
@@ -69,6 +69,41 @@ def get():
     
     try:
         cursor.execute(query)
+        rows = cursor.fetchall()  # Recupera tutte le righe del risultato della query
+        conn.close()
+        
+        # Nomi delle colonne come chiavi del dizionario
+        column_names = ['peso_totale', 'peso_b1', 'peso_b2', 'peso_b3', 'peso_b4', 'peso_b5', 'peso_b6', 'desc', 'priority', 'data']
+        
+        # Conversione delle righe in una lista di dizionari
+        results = [dict(zip(column_names, row)) for row in rows]
+        return results
+    except sqlite3.Error as e:
+        print(f"Errore durante il recupero dei dati: {e}")
+        return []
+    
+import sqlite3
+
+def get_suc(page_number):
+    # Numero di righe per pagina
+    page_size = 100
+    
+    # Calcolo dell'offset
+    offset = (page_number - 1) * page_size
+    
+    # Connessione al database
+    conn = sqlite3.connect(f.get_db())
+    
+    # Creazione di un cursore per eseguire i comandi SQL
+    cursor = conn.cursor()
+    
+    # Query per ottenere le pesate per la pagina richiesta
+    query = '''
+        SELECT * FROM PESATA ORDER BY rowid LIMIT ? OFFSET ?
+    '''
+    
+    try:
+        cursor.execute(query, (page_size, offset))
         rows = cursor.fetchall()  # Recupera tutte le righe del risultato della query
         conn.close()
         
