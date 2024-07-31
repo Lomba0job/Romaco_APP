df703dd54e485a7b9b1a103e60504b0de9967b6d
Michele Lombardi <michelelombardi.job@gmail.com>
2024-07-31 23:15:51 +0200

    aggiutan ricerca con priorità

--- API/API_db.py
+++ API/API_db.py
@@ -55,41 +55,12 @@ def put(peso_tot, b1, b2, b3, b4, b5, b6, desc, prio, data):
         print(f"Errore durante l'inserimento dei dati: {e}")
         return 1
 
-def get():
-    # Connessione al database
-    conn = sqlite3.connect('pesata.db')
-    
-    # Creazione di un cursore per eseguire i comandi SQL
-    cursor = conn.cursor()
-    
-    # Query per ottenere le ultime 100 misure, ordinando per una colonna di riferimento
-    query = '''
-        SELECT * FROM PESATA ORDER BY rowid DESC LIMIT 100
-    '''
-    
-    try:
-        cursor.execute(query)
-        rows = cursor.fetchall()  # Recupera tutte le righe del risultato della query
-        conn.close()
-        
-        # Nomi delle colonne come chiavi del dizionario
-        column_names = ['peso_totale', 'peso_b1', 'peso_b2', 'peso_b3', 'peso_b4', 'peso_b5', 'peso_b6', 'desc', 'priority', 'data']
-        
-        # Conversione delle righe in una lista di dizionari
-        results = [dict(zip(column_names, row)) for row in rows]
-        return results
-    except sqlite3.Error as e:
-        print(f"Errore durante il recupero dei dati: {e}")
-        return []
-    
-import sqlite3
-
-def get_suc(page_number):
+def get(page_number):
     # Numero di righe per pagina
     page_size = 100
     
     # Calcolo dell'offset
-    offset = (page_number - 1) * page_size
+    offset = (page_number - 1) * page_size if page_number > 0 else 0
     
     # Connessione al database
     conn = sqlite3.connect(f.get_db())
@@ -104,6 +75,40 @@ def get_suc(page_number):
     
     try:
         cursor.execute(query, (page_size, offset))
+        rows = cursor.fetchall()  # Recupera tutte le righe del risultato della query
+        conn.close()
+        
+        # Nomi delle colonne come chiavi del dizionario
+        column_names = ['peso_totale', 'peso_b1', 'peso_b2', 'peso_b3', 'peso_b4', 'peso_b5', 'peso_b6', 'desc', 'priority', 'data']
+        
+        # Conversione delle righe in una lista di dizionari
+        results = [dict(zip(column_names, row)) for row in rows]
+        return results
+    except sqlite3.Error as e:
+        print(f"Errore durante il recupero dei dati: {e}")
+        return []
+    
+
+
+def get_priority(page_number, priority):
+    # Numero di righe per pagina
+    page_size = 100
+    
+    # Calcolo dell'offset
+    offset = (page_number - 1) * page_size if page_number > 0 else 0
+    # Connessione al database
+    conn = sqlite3.connect('pesata.db')
+    
+    # Creazione di un cursore per eseguire i comandi SQL
+    cursor = conn.cursor()
+    
+    # Query per ottenere le pesate per la pagina richiesta, filtrando per priorità
+    query = '''
+        SELECT * FROM PESATA WHERE priority = ? ORDER BY rowid LIMIT ? OFFSET ?
+    '''
+    
+    try:
+        cursor.execute(query, (priority, page_size, offset))
         rows = cursor.fetchall()  # Recupera tutte le righe del risultato della query
         conn.close()
         
