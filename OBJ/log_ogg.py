b5db41a0c1b54dc332ababb1f2ddf53753b72591
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-01 15:47:50 +0200

    stabile oggetto grafico

--- OBJ/log_ogg.py
+++ OBJ/log_ogg.py
@@ -7,59 +7,69 @@ class LogEntryWidget(QWidget):
     def __init__(self, date, weight, priority, priority_level):
         super().__init__()
         self.setFixedHeight(40)
-        layout = QHBoxLayout()
+        self.setContentsMargins(0, 0, 0, 0)
+        
+        layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
         
-        content_widget = QWidget()
+        content_widget = QWidget(self)
         content_layout = QHBoxLayout(content_widget)
-        content_layout.setContentsMargins(10, 0, 10, 0)
+        content_layout.setContentsMargins(10, 0, 0, 0)
         content_layout.setSpacing(0)
         
         # PESATA Label and Value
         date_label = QLabel("PESATA:")
         date_label.setObjectName("label")
-        layout.addWidget(date_label)
-        layout.addSpacing(10)
+        content_layout.addWidget(date_label)
+        content_layout.addSpacing(10)
+        
         date_value = QLabel(date)
         date_value.setObjectName("value")
-        layout.addWidget(date_value)
+        content_layout.addWidget(date_value)
         
-        layout.addStretch()
+        content_layout.addStretch()
         
         # PESO TOTALE Label and Value
         weight_label = QLabel("PESO TOTALE:")
         weight_label.setObjectName("label")
-        layout.addWidget(weight_label)
-        layout.addSpacing(10)
+        content_layout.addWidget(weight_label)
+        content_layout.addSpacing(10)
+        
         weight_value = QLabel(weight)
         weight_value.setObjectName("value")
-        layout.addWidget(weight_value)
+        content_layout.addWidget(weight_value)
         
-        layout.addStretch()
+        content_layout.addStretch()
         
         # PRIORITÁ Label and Value
         priority_label = QLabel("PRIORITÁ:")
         priority_label.setObjectName("label")
-        layout.addWidget(priority_label)
-        layout.addSpacing(10)
-        priority_value = QLabel(priority)
-        priority_value.setObjectName("value")
-        layout.addWidget(priority_value)
+        content_layout.addWidget(priority_label)
+        content_layout.addSpacing(10)
         
+        priority_value = QLabel(priority)
+        self.set_fixed_length_text(priority_value, priority, 20)
+        priority_value.setObjectName("value")
+        priority_value.setMinimumWidth(120)
+        priority_value.setMaximumWidth(120)
+        content_layout.addWidget(priority_value)
+        content_layout.addSpacing(20)
         
         layout.addWidget(content_widget)
         
+        
+        # Color indicator
         color_widget = QWidget()
         color_widget.setFixedSize(40, 40)
         color_widget.setObjectName("colorIndicator")
         layout.addWidget(color_widget)
         
         self.setLayout(layout)
-        # self.load_stylesheet()
         self.setAutoFillBackground(True)
         self.set_background_color(QColor(241, 241, 241))
         self.set_priority_color(color_widget, priority_level)
+    
     
     def set_priority_color(self, widget, priority_level):
         colors = {
@@ -78,7 +88,7 @@ class LogEntryWidget(QWidget):
         self.setStyleSheet(f"""
             QWidget {{
                 background-color: {color.name()};
-                border: none;
+                
             }}
             QLabel#label {{
                 color: #5D5D5D;
@@ -91,7 +101,13 @@ class LogEntryWidget(QWidget):
                 font-weight: bold;
             }}
         """)
-    
+    def set_fixed_length_text(self, label, text, length):
+        if len(text) > length:
+            text = text[:length]
+        else:
+            text = text.ljust(length)
+        label.setText(text)
+        
     def load_stylesheet(self):
         try:
             with open(f.get_style("log_ogg.qss"), "r") as file:
