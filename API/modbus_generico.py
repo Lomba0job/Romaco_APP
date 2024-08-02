0d7b0fefb76245ea9d7057c0452db03866c939bc
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-02 16:42:14 +0200

    API_MODBUS

--- API/modbus_generico.py
+++ API/modbus_generico.py
@@ -1,9 +1,12 @@
 from API import modbus
 import serial
 import concurrent.futures
+import time
 
 from OBJ import bilancia as b
 from API import modbus_strutture as st
+
+timeout_duration = 10
 
 def configure(port, list_add):
     
@@ -88,3 +91,101 @@ def scan_modbus_network(port):
     
     return connected_ids
 
+
+
+def tare_command(self, instrument):
+    print("Tare command launched")
+    start_time = time.time()
+    try:
+        
+        tareCoil = instrument.read_bit(st.COIL_TARE_COMMAND, functioncode=1)
+        while(tareCoil==0 and (time.time()-start_time < timeout_duration)):
+            instrument.write_bit(0, 1)
+            tareCoil = instrument.read_bit(st.COIL_TARE_COMMAND, functioncode=1)
+        if(tareCoil==0):
+            print("Timeout exceeded!")
+            return -1
+        time.sleep(2)   #BRUTTO: last command succeed
+        #Here tare takes place
+        tareCoil = instrument.read_bit(st.COIL_TARE_COMMAND, functioncode=1)
+        while(tareCoil==1 and (time.time()-start_time < timeout_duration)):
+            instrument.write_bit(st.COIL_TARE_COMMAND, 0)
+            tareCoil = instrument.read_bit(st.COIL_TARE_COMMAND, functioncode=1)
+        if(tareCoil==1):
+            print("Timeout exceeded!")
+            return -1
+        return 0
+    except Exception as e:
+        print(e)
+        return -1
+    
+def calib_command(self, weight,  instrument):
+    start_time = time.time()
+    try:
+        #Setting the weight
+        calibWeight = instrument.read_register(st.HOLDING_PESO_CALIB_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_PESO_CALIB_LS, functioncode=3)
+        while(calibWeight!=weight and (time.time()-start_time < timeout_duration)):
+            LS16bit = weight & 0xffff
+            MS16bit = (weight >> 16) & 0xffff
+            instrument.write_registers(st.HOLDING_PESO_CALIB_MS, [MS16bit, LS16bit]) 
+            instrument.write_register(st.HOLDING_PESO_CALIB_MS, MS16bit, functioncode=6)  
+            instrument.write_register(st.HOLDING_PESO_CALIB_LS, LS16bit, functioncode=6)  
+            
+            calibWeight = instrument.read_register(st.HOLDING_PESO_CALIB_MS)* 65536 + self.instrument.read_register(st.HOLDING_PESO_CALIB_LS, functioncode=3)
+            
+            t1 = instrument.read_register(st.HOLDING_PESO_CALIB_MS, functioncode=3)* 65536 
+            t2 = instrument.read_register(st.HOLDING_PESO_CALIB_LS, functioncode=3)
+        if(calibWeight!=weight):
+            return -1
+        calibCoil = instrument.read_bit(8, functioncode=1)
+        while(calibCoil==0 and (time.time()-start_time < timeout_duration)):
+            instrument.write_bit(8, 1)
+            calibCoil = instrument.read_bit(8, functioncode=1)
+        if(calibCoil==0):
+            return -1
+        time.sleep(2)   #BRUTTO: last command succeed
+        #Here calib takes place
+        calibCoil = instrument.read_bit(st.COIL_CALIB_COMMAND, functioncode=1)
+        while(calibCoil==1 and (time.time()-start_time < timeout_duration)):
+            instrument.write_bit(st.COIL_CALIB_COMMAND, 0)
+            calibCoil = instrument.read_bit(st.COIL_CALIB_COMMAND, functioncode=1)
+        if(calibCoil==1):
+            return -1
+        return 0
+    except Exception as e:
+        print(e)
+        return -1
+    
+    
+def get_totWeight(self, instrument):
+    try:
+        return instrument.read_register(st.HOLDING_PESO_TOT_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_PESO_TOT_LS, functioncode=3)
+    except:
+        return -1
+    
+def get_cellWeight(self, instrument):
+    cells = []
+    try:
+        b1 = instrument.read_register(st.HOLDING_CELL1_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_CELL1_LS, functioncode=3)
+        b2 = instrument.read_register(st.HOLDING_CELL2_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_CELL2_LS, functioncode=3)
+        b3 = instrument.read_register(st.HOLDING_CELL3_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_CELL3_LS, functioncode=3)
+        b4 = instrument.read_register(st.HOLDING_CELL4_MS, functioncode=3)*65536 + self.instrument.read_register(st.HOLDING_CELL4_LS, functioncode=3)
+        cells.append(b1)
+        cells.append(b2)
+        cells.append(b3)
+        cells.append(b4)
+        return cells
+    except:
+        return -1
+    
+def get_cells_status(self):
+    try:
+        return self.instrument.read_bit(st.COIL_CELL_STATUS, functioncode=1)
+    except:
+        return -1
+    
+def get_adcs_status(self):
+    try:
+        return self.instrument.read_bit(st.COIL_ADCS_STATUS, functioncode=1)
+    except:
+        return -1
