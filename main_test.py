d8b21bf6829c71a692826c59a24a70a9de3efbb9
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-06 11:04:01 +0200

    stabile :

--- main_test.py
+++ main_test.py
@@ -3,26 +3,76 @@ from PyQt6.QtCore import Qt, QEvent, QFile, QTextStream, QDateTime, pyqtSignal, 
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QScrollArea, QHBoxLayout, QDialog, QGridLayout, QStackedWidget, QMenuBar
 )
-from PyQt6.QtGui import QPixmap, QAction, QGuiApplication
+from PyQt6.QtGui import QPixmap, QAction, QGuiApplication, QColor
 
-from PAGE import home_page as h, launcher_page as l, salva_peso_page as s, log_page as lo, graph_page as gh
-from CMP import navbar as nv
+
+from API import funzioni as f 
+from PAGE import home_page as h, launcher_page as l, salva_peso_page as s, log_page as lo
+from CMP import navbar as nv, Bilancia_diagno as b
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Pagina di TEST")
-        
+        # Set the window title
+        self.setWindowTitle("Diagnostica")
+        self.setWindowTitle("RESPONSE ANALYZE APP")
         screen_geometry = QApplication.primaryScreen().geometry()
         self.screen_width = screen_geometry.width()
         self.screen_height = screen_geometry.height()
 
         self.setGeometry(0, 0, self.screen_width, self.screen_height)
+        # Set the central widget
         
-        ogg = gh.Graph_page(self)
+        central_widget = QWidget()
         
-        self.setCentralWidget(ogg)
+        main_layout = QHBoxLayout()
+        main_layout.setSpacing(0)
+        v1 = QVBoxLayout(central_widget)
+
+        # Create and add 6 instances of Home_Page
+        for i in range(1, 7):
+            raggruppa = QWidget()
+            l0 = QHBoxLayout()
+            l0.setSpacing(0)
+            
+            home_page = b.Bilancia(i, self.screen_width-10, self.screen_height-10)
+            home_page.setObjectName("weed")
+            
+            l0.addWidget(home_page)
+            l0.setContentsMargins(2,2,2,2)
+            raggruppa.setLayout(l0)
+            
+            
+            raggruppa.setObjectName("bil")
+            raggruppa.setContentsMargins(1,1,1,1)
+            
+            main_layout.addWidget(raggruppa)
+            
+            
+        self.setStyleSheet("""
+            QWidget#bil{
+                border: 1px solid grey;
+                border-radius: 9px;
+            }
+        """)
+            
+        v1.addStretch()
+        v1.setContentsMargins(0,0,0,0)
+        v1.addLayout(main_layout)
+        v1.addSpacing(10)
+        
+        self.setCentralWidget(central_widget)
+        self.setAutoFillBackground(True)
+        self.set_background_color()
+    
+    def set_background_color(self):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor.fromRgb(241,241,241))
+        self.setPalette(p)
+        # Load the stylesheet
+        
+        
         
     
 if __name__ == "__main__":
