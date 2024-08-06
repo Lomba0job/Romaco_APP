29ae12d7f44352a4e897e6fd1cf108843708b2ac
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-06 02:41:11 +0200

    widnget ui finito

--- CMP/Bilancia_diagno.py
+++ CMP/Bilancia_diagno.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt6.QtCore import Qt, QFile, QTextStream, QSize, pyqtSignal
 from PyQt6.QtWidgets import (
-    QApplication, QPushButton, QLabel, QSizePolicy, QVBoxLayout, QWidget, QScrollArea, QFrame, QHBoxLayout, QDialog, QGridLayout, QStackedWidget, QMenuBar
+    QApplication, QPushButton, QLabel, QSizePolicy, QVBoxLayout, QWidget, QScrollArea, QFrame, QHBoxLayout, QDialog, QGridLayout, QStackedWidget, QMenuBar, 
+    QDoubleSpinBox
+    
 )
 from PyQt6.QtGui import QPixmap, QAction, QIcon
 import os 
@@ -11,7 +13,7 @@ from API import funzioni as f
 
 class Home_Page(QWidget):
 
-    def __init__(self, numero_bilancia: int):
+    def __init__(self, numero_bilancia: int, screen_width):
         super().__init__()
         
         layout = QVBoxLayout()
@@ -23,7 +25,9 @@ class Home_Page(QWidget):
         
         logo_label = QLabel()
         ico = f.get_img("logo.jpg")
-        logo_label.setPixmap(QPixmap(ico).scaled(50, 50, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        larghezza = screen_width / 12
+        altezza = larghezza / 2
+        logo_label.setPixmap(QPixmap(ico).scaled(larghezza, altezza, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
         
         h0.addWidget(logo_label)
         
@@ -39,6 +43,8 @@ class Home_Page(QWidget):
         
         h0.addLayout(v0)
         
+        layout.addLayout(h0)
+        layout.addStretch()
         
         #SECONDO LAYOUT 
         v1 = QVBoxLayout()
@@ -71,10 +77,41 @@ class Home_Page(QWidget):
         v1.addLayout(h2)
         v1.addLayout(h3)
         
+        layout.addLayout(v1)
+        layout.addStretch()
         
+        #TERZO LAYOUT 
+        tara_singola = QPushButton("TARA")
+        tara_singola.setObjectName("tara")
         
+        layout.addWidget(tara_singola)
+        layout.addStretch()
         
-        layout.addWidget(logo_label)
+        v3 = QVBoxLayout()
+        
+        calib_label = QLabel("PESO DI CALIBRAZIONE")
+        calib_label.setObjectName("stato_label")
+        v3.addWidget(calib_label)
+        
+        h4 = QHBoxLayout()
+        self.peso_calib = QDoubleSpinBox(self)
+        self.peso_calib.setRange(0.0, 100.0)  # Imposta il range minimo e massimo
+        self.peso_calib.setDecimals(2)        # Imposta il numero di decimali
+        self.peso_calib.setValue(5.00)         # Imposta il valore iniziale
+        self.peso_calib.setSingleStep(0.1)    # Imposta l'incremento per ogni passo
+        
+        indice = QLabel("Kg")
+        indice.setObjectName("indice")
+        h4.addWidget(self.peso_calib)
+        h4.addWidget(indice)
+        v3.addLayout(h4)
+        
+        calib_singola = QPushButton("CALIBRAZIONE")
+        calib_singola.setObjectName("tara")
+        v3.addWidget(calib_singola)
+        
+        layout.addLayout(v3)
+        
         
         
         
