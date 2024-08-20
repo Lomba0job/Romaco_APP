18d4582c71b25fa07acea76b46574be959e4edbe
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-20 17:19:13 +0200

    pagine e accesso

--- PAGE/setting_page.py
+++ PAGE/setting_page.py
@@ -1,8 +1,8 @@
 import concurrent.futures
 import threading
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QComboBox, QPushButton, QSizePolicy, QProgressBar
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QComboBox, QPushButton, QSizePolicy, QProgressBar, QLineEdit
 from PyQt6.QtCore import Qt, QFile, QTextStream, QThread, pyqtSignal, pyqtSlot, QTimer, QSize
-from PyQt6.QtGui import QColor, QPalette, QPixmap
+from PyQt6.QtGui import QColor, QPalette, QPixmap, QIcon
 import time 
 
 from CMP import rectangle as r, loading2 as carica
@@ -16,10 +16,15 @@ class Settings(QWidget):
         self.master = master
         self.master.setWindowTitle("Settings")
         
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(0,0,0,0)
+        self.main_layout.setSpacing(0)
         
         self.preUI()
         self.setAutoFillBackground(True)
         self.set_background_color()
+        
+        
     
     def set_background_color(self):
         p = self.palette()
@@ -37,7 +42,7 @@ class Settings(QWidget):
             self.setStyleSheet(style_sheet)
             
             
-    def header(self):
+    def header(self, livello):
         
         # Header
         header_layout = QHBoxLayout()
@@ -66,19 +71,137 @@ class Settings(QWidget):
         wid.setMaximumHeight(100)
         wid.setAutoFillBackground(True)
         self.main_layout.addWidget(wid)
+        exit = QPushButton()
+        # Carica la pixmap
+        logo_pixmap = QPixmap(f.get_img("back.png"))
+        
+        # Crea un'icona a partire dalla pixmap
+        icon = QIcon(logo_pixmap)
+        
+        # Imposta l'icona sul pulsante
+        exit.setIcon(icon)
+        
+        # Imposta la dimensione dell'icona sul pulsante (se necessario)
+        exit.setIconSize(logo_pixmap.scaledToHeight(50).size())
+        exit.setMaximumWidth(70)
+        exit.clicked.connect(self.master.back_home)
+        if( livello == 0):
+            self.main_layout.addWidget(exit)
+        elif livello == 1: 
+            h1 = QHBoxLayout()
+            contro_conf = QLabel("LIVELLO 1   ")
+            contro_conf.setObjectName("configurazione_label")
+            contro_conf.setAlignment(Qt.AlignmentFlag.AlignTop)
+            h1.addWidget(exit)
+            h1.addStretch()
+            h1.addWidget(contro_conf)
+            self.main_layout.addLayout(h1)
+        elif livello == 2: 
+            h1 = QHBoxLayout()
+            contro_conf = QLabel("LIVELLO 2   ")
+            contro_conf.setObjectName("configurazione_label")
+            contro_conf.setAlignment(Qt.AlignmentFlag.AlignTop)
+            h1.addWidget(exit)
+            h1.addStretch()
+            h1.addWidget(contro_conf)
+            self.main_layout.addLayout(h1)
+            
+        self.main_layout.addStretch()
+    def log(self):
+        
+        
+        
+        wid = QWidget()
+        ve = QVBoxLayout()
+        h = QHBoxLayout()
+        
+        titolo = QLabel("AREA PRIVATA")
+        titolo.setObjectName("titoloi")
+        password = QLabel("password")
+        password.setObjectName("pass")
+        self.pass_text = QLineEdit()
+        self.pass_text.setEchoMode(QLineEdit.EchoMode.Password)
+        # Collegare il segnale returnPressed al metodo enter
+        self.pass_text.returnPressed.connect(self.enter)
+        
+        
+        x = QPushButton()
+        logo_pixmap = QPixmap(f.get_img("X.png")) # Carica la pixmap
+        icon = QIcon(logo_pixmap) # Crea un'icona a partire dalla pixmap
+        x.setIcon(icon) # Imposta l'icona sul pulsante
+        # Imposta la dimensione dell'icona sul pulsante (se necessario)
+        x.setIconSize(logo_pixmap.scaledToHeight(40).size())
+        x.setMaximumWidth(50)
+        x.clicked.connect(self.canc)
+        
+        v = QPushButton()
+        logo_pixmap = QPixmap(f.get_img("V.png"))  # Carica la pixmap
+        icon = QIcon(logo_pixmap) # Crea un'icona a partire dalla pixmap
+        v.setIcon(icon) # Imposta l'icona sul pulsante
+        # Imposta la dimensione dell'icona sul pulsante (se necessario)
+        v.setIconSize(logo_pixmap.scaledToHeight(40).size())
+        v.setMaximumWidth(50)
+        v.clicked.connect(self.enter)
+        
+        h.addStretch()
+        h.addWidget(x)
+        h.addWidget(v)
+        
+        titolo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        password.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        
+        ve.addWidget(titolo)
+        ve.addWidget(password)
+        ve.addWidget(self.pass_text)
+        ve.addLayout(h)
+        
+        wid.setLayout(ve)
+        wid.setObjectName("wid")
+        wid.setMaximumWidth(600)
+        
+        
+        h2 = QHBoxLayout()
+        h2.addStretch()
+        h2.addWidget(wid)
+        h2.addStretch()
+        self.main_layout.addLayout(h2)
+        self.main_layout.addStretch()
     
     def preUI(self):
-        self.main_layout = QVBoxLayout()
-        self.main_layout.setContentsMargins(0,0,0,0)
-        self.main_layout.setSpacing(0)
-        self.header()
+        self.header(0)
+        self.log()
         
-        
-        exit = QPushButton("esci")
-        exit.setObjectName("puls")
-        exit.clicked.connect(self.master.back_home)
-        self.main_layout.addStretch()
-        self.main_layout.addWidget(exit)
-        self.main_layout.addStretch()
         self.setLayout(self.main_layout)
         self.load_stylesheet()
+        
+    def UIlivello2(self):
+        self.clearLayout(self.main_layout)
+        self.header(2)
+        
+    def UIlivello1(self):
+        self.clearLayout(self.main_layout)
+        self.header(1)
+
+    def canc(self):
+        self.pass_text.setText("")
+        
+    def enter(self):
+        if self.pass_text.text() == "IsolaNLV":
+            print("livello1")
+            self.UIlivello1()
+        elif self.pass_text.text() == "NLVlombapass":
+            print("livello2")
+            self.UIlivello2()
+        else:
+            print("WRONG")
+        self.canc()
+        
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearLayout(item.layout())
