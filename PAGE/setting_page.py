03c32ff2c2a04dbc3577ec463be1fda9466e5f5c
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-21 15:18:11 +0200

    stabile multi page

--- PAGE/setting_page.py
+++ PAGE/setting_page.py
@@ -1,50 +1,19 @@
 import concurrent.futures
 import threading
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QComboBox, QPushButton, QSizePolicy, QProgressBar, QLineEdit
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QComboBox, QPushButton, QSizePolicy, QProgressBar, QLineEdit, QStackedWidget
 from PyQt6.QtCore import Qt, QFile, QTextStream, QThread, pyqtSignal, pyqtSlot, QTimer, QSize
 from PyQt6.QtGui import QColor, QPalette, QPixmap, QIcon
-import time 
 
 from CMP import rectangle as r, loading2 as carica, livello1 as l1, livello2 as l2
 from API import funzioni as f, modbus_generico as mb
 
-class Settings(QWidget):
 
+class Header(QWidget):
     def __init__(self, master):
-        super().__init__()
-
+       
+        super().__init__() 
         self.master = master
-        self.master.setWindowTitle("Settings")
         
-        self.main_layout = QVBoxLayout()
-        self.main_layout.setContentsMargins(0,0,0,0)
-        self.main_layout.setSpacing(0)
-        
-        self.preUI()
-        self.setAutoFillBackground(True)
-        self.set_background_color()
-        
-        
-    
-    def set_background_color(self):
-        p = self.palette()
-        p.setColor(self.backgroundRole(), QColor.fromRgb(254,254,254))
-        self.setPalette(p)
-        # Load the stylesheet
-        self.load_stylesheet()
-
-    def load_stylesheet(self):
-        file = QFile(f.get_style("settings.qss"))
-        if file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text):
-            stream = QTextStream(file)
-            style_sheet = stream.readAll()
-            file.close()
-            self.setStyleSheet(style_sheet)
-            
-            
-    def header(self):
-        
-        # Header
         header_layout = QHBoxLayout()
         logo_label = QLabel()
         ico = f.get_img("logo.jpg")
@@ -65,24 +34,19 @@ class Settings(QWidget):
         configurazione_label.setObjectName("configurazione_label")
         header_layout.addWidget(configurazione_label)
 
-        wid = QWidget()
-        wid.setObjectName("menulab")
-        wid.setLayout(header_layout)
-        wid.setMaximumHeight(100)
-        wid.setAutoFillBackground(True)
-        self.main_layout.addWidget(wid)
-        self.sotto_menu = QVBoxLayout()
-        
         self.h1 = QHBoxLayout()
-        self.contro_label(0)
-        self.main_layout.addLayout(self.h1)
-        self.main_layout.addStretch()  
-        self.sotto_menu.setContentsMargins(0,0,0,0)
-        self.sotto_menu.setSpacing(0)
-        self.main_layout.addLayout(self.sotto_menu)
-        self.main_layout.addStretch()
-    
-    def contro_label(self, livello):
+        self.contro_label(0, master)
+        
+        mast_l = QVBoxLayout()
+        mast_l.addLayout(header_layout)
+        mast_l.addLayout(self.h1)
+        
+        self.setLayout(mast_l)
+        self.setMaximumHeight(150)
+        self.setAutoFillBackground(True)
+        self.set_background_color()
+        
+    def contro_label(self, livello, master):
         exit = QPushButton()
         # Carica la pixmap
         logo_pixmap = QPixmap(f.get_img("back.png"))
@@ -101,7 +65,7 @@ class Settings(QWidget):
             self.clearLayout(self.h1)
             self.h1.addWidget(exit)
             self.h1.addStretch()
-            exit.clicked.connect(self.back_home)
+            exit.clicked.connect(master.back_home)
         elif livello == 1: 
             self.clearLayout(self.h1)
             contro_conf = QLabel("LIVELLO 1   ")
@@ -110,7 +74,7 @@ class Settings(QWidget):
             self.h1.addWidget(exit)
             self.h1.addStretch()
             self.h1.addWidget(contro_conf)
-            exit.clicked.connect(self.back_setting)
+            exit.clicked.connect(master.back_setting)
         elif livello == 2: 
             self.clearLayout(self.h1)
             contro_conf = QLabel("LIVELLO 2   ")
@@ -119,7 +83,7 @@ class Settings(QWidget):
             self.h1.addWidget(exit)
             self.h1.addStretch()
             self.h1.addWidget(contro_conf)
-            exit.clicked.connect(self.back_setting)
+            exit.clicked.connect(master.back_setting)
         elif livello == 3: 
             self.clearLayout(self.h1)
             contro_conf = QLabel("Misurazione Contiuna")
@@ -128,13 +92,39 @@ class Settings(QWidget):
             self.h1.addWidget(exit)
             self.h1.addStretch()
             self.h1.addWidget(contro_conf)
-            exit.clicked.connect(self.back_liv2)
+            exit.clicked.connect(master.back_liv2)
         
-        
-        
-        
-    def log(self):
-        
+    def set_background_color(self):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor.fromRgb(254,254,254))
+        self.setPalette(p)
+        self.load_stylesheet()
+
+    def load_stylesheet(self):
+        file = QFile(f.get_style("settings.qss"))
+        if file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text):
+            stream = QTextStream(file)
+            style_sheet = stream.readAll()
+            file.close()
+            self.setStyleSheet(style_sheet)
+    
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearLayout(item.layout())
+
+
+class Home_Impo(QWidget):
+    
+    def __init__(self, master):
+       
+        super().__init__() 
+        self.master = master
         wid = QWidget()
         ve = QVBoxLayout()
         h = QHBoxLayout()
@@ -184,75 +174,109 @@ class Settings(QWidget):
         wid.setMaximumWidth(600)
         
         
-        h2 = QHBoxLayout()
-        h2.addStretch()
-        h2.addWidget(wid)
-        h2.addStretch()
-        self.sotto_menu.addLayout(h2)
-        self.sotto_menu.addStretch()
-    
-    def preUI(self):
-        self.header()
-        self.log()
+        l = QVBoxLayout()
+        l.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        l.addStretch()
+        l.addWidget(wid)
+        l.addStretch()
         
-        self.setLayout(self.main_layout)
+        self.setLayout(l)
+        self.setAutoFillBackground(True)
+        self.set_background_color()
+        
+    def set_background_color(self):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor.fromRgb(254,254,254))
+        self.setPalette(p)
         self.load_stylesheet()
-        
-    def UIlivello2(self):
-        self.clearLayout(self.sotto_menu)
-        self.contro_label(2)
-        self.liv2 = l2.Livello2(self)
-        self.sotto_menu.addWidget(self.liv2)
-        self.sotto_menu.addStretch()
-        
-    def diagnosi_page(self):
-        self.contro_label(3)
-        
-    def UIlivello1(self):
-        self.clearLayout(self.sotto_menu)
-        self.contro_label(1)
-        self.liv1 = l1.Livello1(self)
-        self.sotto_menu.addWidget(self.liv1)
-        self.sotto_menu.addStretch()
-        
 
-    def canc(self):
-        self.pass_text.setText("")
+    def load_stylesheet(self):
+        file = QFile(f.get_style("settings.qss"))
+        if file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text):
+            stream = QTextStream(file)
+            style_sheet = stream.readAll()
+            file.close()
+            self.setStyleSheet(style_sheet)
         
     def enter(self):
         if self.pass_text.text() == "IsolaNLV":
-            print("livello1")
-            self.UIlivello1()
+            self.master.stacked_widget.setCurrentIndex(1)  # Livello1
         elif self.pass_text.text() == "NLVlombapass":
-            print("livello2")
-            self.UIlivello2()
+            self.master.stacked_widget.setCurrentIndex(2)  # Livello2
         else:
             print("WRONG")
         self.canc()
         
-    def clearLayout(self, layout):
-        if layout is not None:
-            while layout.count():
-                item = layout.takeAt(0)
-                widget = item.widget()
-                if widget is not None:
-                    widget.deleteLater()
-                else:
-                    self.clearLayout(item.layout())
-                    
-    def back_home(self):
-        self.master.back_home()
+    def canc(self):
+        self.pass_text.setText("")
         
+        
+
+class Settings(QWidget):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+        self.master.setWindowTitle("Settings")
+        
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(0,0,0,0)
+        self.main_layout.setSpacing(0)
+        
+        self.head = Header(self)
+        self.main_layout.addWidget(self.head)
+        
+        self.stacked_widget = QStackedWidget()
+        self.main_layout.addWidget(self.stacked_widget)
+        
+        self.preUI()
+        self.setLayout(self.main_layout)
+        self.setAutoFillBackground(True)
+        self.set_background_color()
+        
+    def set_background_color(self):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor.fromRgb(254,254,254))
+        self.setPalette(p)
+        self.load_stylesheet()
+
+    def load_stylesheet(self):
+        file = QFile(f.get_style("settings.qss"))
+        if file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text):
+            stream = QTextStream(file)
+            style_sheet = stream.readAll()
+            file.close()
+            self.setStyleSheet(style_sheet)
+
+    def preUI(self):
+    
+        self.home_page = Home_Impo(self)
+        self.stacked_widget.addWidget(self.home_page)
+        
+        self.liv1_page = l1.Livello1(self)
+        self.stacked_widget.addWidget(self.liv1_page)
+        
+        self.liv2_page = l2.Livello2(self)
+        self.stacked_widget.addWidget(self.liv2_page)
+        
+        self.stacked_widget.setCurrentIndex(0)
+        
+    
+    def back_home(self):
+        self.master.back_home()  # Home
         
     def back_setting(self):
         self.contro_label(0)
-        self.clearLayout(self.main_layout) 
-        self.preUI()
+        self.stacked_widget.setCurrentIndex(0)  # Home
         
     def back_liv2(self):
         self.contro_label(2)
-        self.liv2.home()
+        self.liv2_page.home()
+        self.stacked_widget.setCurrentIndex(2)  # Livello2
         
     def back_liv1(self):
         self.contro_label(1)
-        self.liv1.home()
+        self.liv1_page.home()
+        self.stacked_widget.setCurrentIndex(1)  # Livello1
+
+    def contro_label(self, livello):
+        self.head.contro_label(livello, self)
