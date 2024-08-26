60f340c32532ce525c639da2053409392be1672a
Michele Lombardi <michelelombardi.job@gmail.com>
2024-08-26 11:11:04 +0200

    test

---
+++ test/vtk.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+import vtk
+
+os.environ['QT_QPA_PLATFORM'] = 'xcb'
+os.environ['VTK_USE_X'] = '1'
+
+class SimpleVTKWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.vtkWidget = QVTKRenderWindowInteractor(self)
+        self.layout.addWidget(self.vtkWidget)
+
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(1, 1, 1)
+        self.render_window = self.vtkWidget.GetRenderWindow()
+        self.render_window.AddRenderer(self.renderer)
+
+        self.interactor = self.render_window.GetInteractor()
+        self.interactor.Initialize()
+        self.interactor.Start()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = QMainWindow()
+    main_window.setCentralWidget(SimpleVTKWidget())
+    main_window.resize(800, 600)
+    main_window.show()
+    sys.exit(app.exec())
